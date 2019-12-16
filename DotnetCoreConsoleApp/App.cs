b61d61a15c96696d2f8619ef28c995cbc41bd229
XUJINKAI <xujinkai@github.com>
2019-12-16 09:17:46 +0800

    dotnet core console app

---
+++ DotnetCoreConsoleApp/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandDotNet.Attributes;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace DotnetCoreConsoleApp
+{
+    class App
+    {
+        public void Test()
+        {
+            Console.WriteLine("Hello World!", Color.Red);
+        }
+    }
+}
---
+++ DotnetCoreConsoleApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using CommandDotNet;
+using CommandDotNet.Models;
+using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace DotnetCoreConsoleApp
+{
+    class Program
+    {
+        public static AppRunner<App> AppRunner;
+
+        static void Main(string[] args)
+        {
+            Console.WriteAscii("ConsoleApp", Color.Red);
+
+            var setting = new AppSettings()
+            {
+                ThrowOnUnexpectedArgument = true,
+                Help = new AppHelpSettings()
+                {
+                    UsageAppNameStyle = UsageAppNameStyle.Executable,
+                },
+            };
+            AppRunner = new AppRunner<App>(setting);
+
+            AppRunner.Run(args);
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                args = CommandLineToArgs(line);
+                AppRunner.Run(args);
+            }
+        }
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW([MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
+
+        static string[] CommandLineToArgs(string commandLine)
+        {
+            if (string.IsNullOrEmpty(commandLine))
+                return Array.Empty<string>();
+
+            var argv = CommandLineToArgvW(commandLine, out int argc);
+            if (argv == IntPtr.Zero)
+                throw new System.ComponentModel.Win32Exception();
+
+            try
+            {
+                var args = new string[argc];
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    args[i] = Marshal.PtrToStringUni(p);
+                }
+
+                return args;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(argv);
+            }
+        }
+    }
+}
